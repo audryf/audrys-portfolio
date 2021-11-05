718fd21cf4ade52ab655a21546502d56b44ade26
audryf <84256033+audryf@users.noreply.github.com>
2021-11-04 20:27:10 -0500

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,22 @@
 import React from "react";
+import githubLogo from "../../assets/images/logos/github-icon.png";
+import linkedInLogo from "../../assets/images/logos/linkedin-logo.png";
 
-function Footer () {
+function Footer() {
 
     return (
         <footer className="text-center pt-5 mt-5">
-            Copyright &copy; {new Date().getFullYear()}
+            <div>
+                Copyright &copy; {new Date().getFullYear()}
+            </div>
+            <div className="row mt-5">
+                <a className="col-6 text-end" target="_blank" href="https://github.com/audryf">
+                    <img src={githubLogo} alt="github icon" />
+                </a>
+                <a className="col-6 text-start" target="_blank" href="https://www.linkedin.com/in/audry-ford">
+                    <img src={linkedInLogo} alt="linkedIn icon" />
+                </a>
+            </div>
         </footer>
     )
 }
