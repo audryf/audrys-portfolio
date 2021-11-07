72012832cad4cd468c045f91c0dba962f96bd0d6
audryf <84256033+audryf@users.noreply.github.com>
2021-11-07 13:44:03 -0600

    about nav

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -13,7 +13,7 @@ function Navigation() {
         <div className="navbar-collapse flex-row-reverse" id="navbarNav">
           <ul className="navbar-nav fs-3">
             <li className="nav-item m-2 nav-button nav-active">
-              <Link to="/" className="nav-link main-text-color" aria-current="page">About Me</Link>
+              <Link to="/audrys-portfolio" className="nav-link main-text-color" aria-current="page">About Me</Link>
             </li>
             <li className="nav-item m-2 nav-button">
               <Link to="/portfolio" className="nav-link main-text-color">Portfolio</Link>
